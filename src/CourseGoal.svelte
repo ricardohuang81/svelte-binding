83295c818721aa0c1e085275acab650719c6d4b7
Ricky Huang <huangricky@gmail.com>
2019-11-22 12:56:22 -0500

    binding assignment changing CSS class based on input

---
+++ src/CourseGoal.svelte
@@ -0,0 +1,15 @@
+<!-- scripts -->
+<script>
+  export let courseGoal;
+  $: turnsRed = courseGoal.includes("!");
+</script>
+
+<!-- styles -->
+<style>
+  .turn-red {
+    color: red;
+  }
+</style>
+
+<!-- markup -->
+<h1 class:turn-red="{turnsRed}">{courseGoal}</h1>
